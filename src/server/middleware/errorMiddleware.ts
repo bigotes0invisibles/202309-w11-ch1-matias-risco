59a282d6e68c5fa58a97ee41afa970fe2592f903
Matias Risco <matiasriscolagos@gmail.com>
2023-11-23 09:45:20 +0100

    Fix imports errors

--- src/server/middleware/errorMiddleware.ts
+++ src/server/middleware/errorMiddleware.ts
@@ -2,7 +2,7 @@ import { type NextFunction } from "connect";
 import { type Request, type Response } from "express";
 import debugCreator from "debug";
 import chalk from "chalk";
-import CustomError from "../../CustomError/CustomError";
+import CustomError from "../../CustomError/CustomError.js";
 
 const debug = debugCreator("app:server:middleware:errorMiddleware");
 
